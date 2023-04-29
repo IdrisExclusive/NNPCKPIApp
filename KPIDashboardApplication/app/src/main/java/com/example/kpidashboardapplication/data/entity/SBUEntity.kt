e09412da184e76c71d0403f6427b0545e1f19457
Idris Ahmad <idris.ahmad@nnpcgroup.com>
2023-04-29 21:41:38 +0100

    First Commit

--- KPIDashboardApplication/app/src/main/java/com/example/kpidashboardapplication/data/entity/SBUEntity.kt
+++ KPIDashboardApplication/app/src/main/java/com/example/kpidashboardapplication/data/entity/SBUEntity.kt
@@ -1,6 +1,16 @@
 package com.example.kpidashboardapplication.data.entity
 
+import androidx.compose.runtime.Immutable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 
 
+@Entity(
+tableName ="KPIMaster"
+)
 class SBUMasterEntity {
+
+
 }
